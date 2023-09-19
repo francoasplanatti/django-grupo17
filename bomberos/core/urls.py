c228cfbd3de6d781db3aec3fe7594ac79577f3a4
Franco Asplanatti <francoasplanatti57@gmail.com>
2023-09-19 01:06:13 -0300

    f-add-filter

--- bomberos/core/urls.py
+++ bomberos/core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('vehiculos', views.vehiculos, name='vehiculos'),
     path('personal', views.personal, name='personal'),
-    re_path(r'personal/(?P<year>\d{4})/$', views.personal_historico, name='personal'),
+    re_path(r'personal/(?P<year>\d{4})/$', views.personal_historico, name='personal_historico'),
 ]
--- bomberos/core/views.py
+++ bomberos/core/views.py
@@ -7,7 +7,21 @@ def personal(request):
     return render(request, "core/personal.html")
 
 def personal_historico(request, year):
-    return render(request, "core/personal_historico.html", {"año": year})
+    listado = [
+        ['Tom Cruise', 'Jefe de área'],
+        ['Leo DiCaprio', 'Bombero'],
+        ['Will Smith', 'Bombero'],
+        ['Vin Diesel', 'Bombero'],
+        ['Ryan Gosling', 'Bombero']
+    ]
+
+    context = {
+        'año': year,
+        'listado_historico': listado,
+        'cant_personal': len(listado),
+    }
+
+    return render(request, "core/personal_historico.html", context)
 
 def vehiculos(request):
     return render(request, "core/vehiculos.html")
